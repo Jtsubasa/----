a86af996c3dfb0e49451b5cd3c9a40e21e96a0a6
Wing2C1 <tsubasa9387@gmail.com>
2024-09-22 09:00:57 +0900

    初期設定

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import pygame as pg
+import pygame
 import tkinter as tk
 from tkinter import *
 
 class main:
     def __init__(self):
         self.window = tk.Tk()
+        self.window.title("玉シミュレーション設定")
+        self.window.config(bg="#87cefa")
+        self.window.geometry("600x400+0+0")
+        self.setup_ui()
+
+    def setup_ui(self):
+        TITLE_FONT = ("Helvetica", 28, "bold")
+        LABEL_FONT = ("Helvetica", 18)
+        BUTTON_FONT = ("Helvetica", 14, "bold")
 
     def run(self):
         self.window.mainloop()
